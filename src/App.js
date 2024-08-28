cbfe6c99c046ade63c524eaf333a929f32dd937b
Chandan Kumar <kumarchandan99990@gmail.com>
2024-08-28 16:14:48 +0530

    Add all files

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Practice from "./Practice"
+function App() {
+  return (
+    <>
+    <Practice />
+    <p>lalit</p>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Practice.js
@@ -0,0 +1,9 @@
+function Practice(){
+    return(
+        <>
+        <h1>Hello world</h1>
+        <h2>hello mere sath chai pioge</h2>
+        </>
+    )
+}
+export default Practice
